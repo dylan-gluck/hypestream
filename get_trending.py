5aa31245e01f4a178c26c00e5444771862f1d46f
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2021-12-15 04:14:52 -0800

    Updated trending script

--- get_trending.py
+++ get_trending.py
@@ -1,7 +1,9 @@
 import os
+import glob
+import random
 import urllib.request
 import ffmpeg
-from TikTokApi import TikTokApi
+from TikTokApi import TikTokApi as tt
 
 # Banner
 print("""
@@ -30,69 +32,72 @@ print("""
 
 """)
 
-# Create API Instance
-print("Fetching Top 10 TikTok Videos")
-api = TikTokApi.get_instance()
+INDEX = input("Series Index: ")
+TITLE = input("Video Title: ")
+VIDEO_TITLE = TITLE + " | HypeStream Comp " + INDEX
+VIDEO_FILE = "./out/tiktok-trending-videos-" + INDEX + ".mp4"
+print("Full Title: " + VIDEO_TITLE)
 
-# Get Videos from API
-trending = api.by_trending(count=10)
+# Create API Instance
+print("Fetching Top 300 TikTok Videos")
+api = tt.get_instance()
+# Get 200 Videos from API
+trending = api.by_trending(count=300)
+# Select Random 30
+print("Selecting Random 30")
+random.shuffle(trending)
 
 # Loop Over Results, Save in Memory video_ids[]
 video_ids = []
-for tiktok in trending:
+for tiktok in trending[0:30]:
     # Create Path
     video_ids.append(tiktok['id'])
     # Download Video
-    print("Downloading Video:" + tiktok['id'])
+    # print("Downloading Video:" + tiktok['id'])
     file_name = "./tmp/" + tiktok['id'] + ".mp4"
     urllib.request.urlretrieve(tiktok['video']['downloadAddr'], file_name)
+print("Download Complete")
 
 # Split Video & Audio
 # Add Padding to Video 16:9
 # Store in Memory _v[]
-print("Processing Videos to 16:9")
+print("Splitting Video and Audio")
 _v = []
-tmp = []
 for _id in video_ids:
     # Format Video to 16:9
     path = "./tmp/" + _id + ".mp4"
     options = {
-        'width':'1820',
-        'height':'1024',
-        'x':'662',
-        'y':'0',
+        'width': '1920',
+        'height': '1080',
+        'x': '(ow-iw)/2',
+        'y': '(oh-ih)/2',
         'color': 'Black'
     }
     video = ffmpeg.input(path)
-    processed = {
-        "v": ffmpeg.filter(video,"pad",**options),
-        "a": video.audio
-    }
-    _v.append(processed)
-    tmp.append(path)
+    # video_scaled = video.video.filter("setsar", sar="1/1").filter("scale", w="-1", h="1080").filter("pad",**options)
+    # processed = {
+    #     "v": video_scaled,
+    #     "a": video.audio
+    # }
+    _v.append(
+        video["v"]
+        .filter("scale", w="-1", h="1080")
+        .filter("pad",**options)
+        .filter("setsar", sar="1/1")
+    )
+    _v.append(video["a"])
     
 # Create Concat Video
 # No other way to do this and get the audio working
 print("Processing Concat Video")
-joined = ffmpeg.concat(
-    _v[0]['v'], _v[0]['a'], 
-    _v[1]['v'], _v[1]['a'], 
-    _v[2]['v'], _v[2]['a'], 
-    _v[3]['v'], _v[3]['a'],
-    _v[4]['v'], _v[4]['a'], 
-    _v[5]['v'], _v[5]['a'], 
-    _v[6]['v'], _v[6]['a'], 
-    _v[7]['v'], _v[7]['a'],
-    _v[8]['v'], _v[8]['a'], 
-    _v[9]['v'], _v[9]['a'], 
-    v=1, a=1
-)
-out_video = ffmpeg.filter(joined.node[0],'fps', fps=24, round='up')
+joined = ffmpeg.concat(*_v, v=1, a=1)
+out_video = joined.node[0].filter('fps', fps=24, round='up').filter("setsar", sar="1/1")
 out_audio = joined.node[1]
-out = ffmpeg.output(out_video, out_audio, "./out/tiktok-top-10.mp4")
+out = ffmpeg.output(out_video, out_audio, VIDEO_FILE)
 out.run(quiet=True, overwrite_output=True)
 
 # Remove Temp Files
+tmp = glob.glob('./tmp/*.mp4')
 for path in tmp:
     os.remove(path)
 
